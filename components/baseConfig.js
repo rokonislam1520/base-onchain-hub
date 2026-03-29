daa6a09c23446f2bc127383b5d9099363d9cb746
R O K O N 💦💭 <130083848+rokonislam1520@users.noreply.github.com>
2026-03-29 13:07:47 +0600

    Delete components/baseConfig.js

--- components/baseConfig.js
+++
@@ -1,4 +0,0 @@
-export const BASE_NETWORKS = {
-  mainnet: { chainId: 8453, name: "Base Mainnet", rpc: "https://mainnet.base.org" },
-  sepolia: { chainId: 84532, name: "Base Sepolia", rpc: "https://sepolia.base.org" }
-};
