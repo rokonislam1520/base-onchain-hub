2c9c95449b2a7c9a08e885c909cecce58a8d1466
R O K O N 💦💭 <130083848+rokonislam1520@users.noreply.github.com>
2026-03-29 11:17:04 +0600

    Create index.js

---
+++ src/pages/src/pages/index.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { SignInWithBase } from '../components/SignInWithBase'; 
+import { MintNFT } from '../components/MintNFT'; // <-- Notun line
+import { ethers } from 'ethers';
+Ebong nicher dike jekhane Base Pay Integration er div-ti shesh hoyeche, tar thik porei ei line-ti bosiye din:
+        <MintNFT /> 
+
+---------------------------------------------------------------------
