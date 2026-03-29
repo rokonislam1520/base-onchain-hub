608a73ad82702fdfb009d6db7bb7a670ce824638
R O K O N 💦💭 <130083848+rokonislam1520@users.noreply.github.com>
2026-03-29 13:05:39 +0600

    Create SuperchainFaucet.js

---
+++ components/SuperchainFaucet.js
@@ -0,0 +1,7 @@
+import React from 'react';
+export const SuperchainFaucet = () => (
+  <div style={{ padding: '15px', background: '#fae8ff', margin: '10px 0', borderRadius: '8px' }}>
+    <h3>Superchain Faucet 🚰</h3>
+    <p>Get free testnet ETH for Base Sepolia from the Superchain Console.</p>
+  </div>
+);
