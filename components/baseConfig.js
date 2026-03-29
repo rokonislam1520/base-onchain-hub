acbda0b20ab5923eba05e8afc7cdb808654f4f14
R O K O N 💦💭 <130083848+rokonislam1520@users.noreply.github.com>
2026-03-29 13:05:57 +0600

    Create baseConfig.js

---
+++ components/baseConfig.js
@@ -0,0 +1,4 @@
+export const BASE_NETWORKS = {
+  mainnet: { chainId: 8453, name: "Base Mainnet", rpc: "https://mainnet.base.org" },
+  sepolia: { chainId: 84532, name: "Base Sepolia", rpc: "https://sepolia.base.org" }
+};
